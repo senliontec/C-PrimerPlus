f3de47b2522e90939c71d74e0116b57d248b3ee2
senliontec <senliontec@163.com>
2021-02-18 11:09:46 +0800

    add the readme file

---
+++ README.md
@@ -0,0 +1,1 @@
+this is the code of c++ primer plus 6
---
+++ chapter2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Come up and C++ me come time. "<<endl;
+
+    return 0;
+
+}
