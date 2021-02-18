7d1d20190b23c7fb4e7943e4bd5d320e211db310
senliontec <senliontec@163.com>
2021-02-18 12:26:10 +0800

    update using

--- myfirst.cpp
+++ myfirst.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
+using std::cout;
+using std::endl;
+using std::cin;
+
+
 int main()
 {
-    using namespace std;
+//    using namespace std;
 
     cout<<"Come up and C++ me come time. ";
     cout <<endl;
