0e06875e7619de60c01b61064d2cf72b4826a119
senlion <senliontec@163.com>
2021-02-19 13:19:17 +0800

    程序清单3.1 关于符号类型长度

--- section3/limits.cpp
+++ section3/limits.cpp
@@ -16,5 +16,36 @@ int main()
 
     using namespace std;
 
+    int n_int = INT_MAX; //initialize n_int to max int value
+
+    short n_short = SHRT_MAX; // symbols defined in climits file
+
+    long n_long = LONG_MAX;
+
+    long long n_llong = LLONG_MAX;
+
+    //sizeof operator yields size of type or of variable
+
+    cout << "int is " << sizeof(int) << " bytes(字节，1字节=8位)." << endl;
+
+    cout << "short is " << sizeof(n_short) << " bytes." << endl;
+
+    cout << "long is " << sizeof(n_long) << " bytes." << endl;
+
+    cout << "long long is " << sizeof(n_llong) << " bytes.\n"
+         << endl;
+
+    //////////////////////////////////////////////////////////////////
+
+    cout << "Maximum values: " << endl;
+    cout << "int: " << n_int << endl;
+    cout << "short: " << n_short << endl;
+    cout << "long: " << n_long << endl;
+    cout << "long long: " << n_llong << endl
+         << endl;
+
+    cout << "Minimum int value = " << INT_MIN << endl;
+    cout << "Bits per byte = " << CHAR_BIT << endl;
+
     return 0;
 }
