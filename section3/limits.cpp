d34cee80fb63d203027628bb8ffa3cb896b8b28c
senlion <senliontec@163.com>
2021-02-19 12:41:13 +0800

    程序清单3.1

--- section3/limits.cpp
+++ section3/limits.cpp
@@ -5,6 +5,12 @@
 #include <iostream>
 #include <climits> //use limits.h for old system
 
+// short 至少16位
+// int 至少与 short 一样长
+// long 至少32位， 且至少与int一样长
+// long long 至少64位，且至少与long一样长
+// 字节(byte)通常指8位的内存单元，字节就是指描述计算机内存量的度量单位
+
 int main()
 {
 
