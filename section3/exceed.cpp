dca3df91a97ecc880ef4a87f4317f17ac5ebd2ea
senlion <senliontec@163.com>
2021-02-19 16:48:43 +0800

    程序清单3.2

--- section3/exceed.cpp
+++ section3/exceed.cpp
@@ -9,40 +9,40 @@
 
 #include <iostream>
 
-#define ZERO 0 // make ZERO symbol for 0 value
-
 #include <climits> // defines INT_MAX as largest int value
+
+#define ZERO 0 // make ZERO symbol for 0 value
 
 int main()
 {
-    using namespace std;
+     using namespace std;
 
-    short sam = SHRT_MAX; // initialize a variable to max value.
+     short sam = SHRT_MAX; // initialize a variable to max value.
 
-    unsigned short sue = sam; // okay if variable sam already defined
+     unsigned short sue = sam; // okay if variable sam already defined
 
-    cout << "Sam has " << sam << " dollars and Sue has  " << sue << " dollars deposited. " << endl
-         << "Add $1 to each account. " << endl
-         << "Now ";
+     cout << "Sam has " << sam << " dollars and Sue has  " << sue << " dollars deposited. " << endl
+          << "Add $1 to each account. " << endl
+          << "Now ";
 
-    sam = sam + 1;
-    sue = sue + 1;
+     sam = sam + 1;
+     sue = sue + 1;
 
-    cout << "Sam has " << sam << " dollars and Sue has " << sue << " dolloars deposited." << endl
-         << "pool Sam!" << endl;
+     cout << "Sam has " << sam << " dollars and Sue has " << sue << " dolloars deposited." << endl
+          << "pool Sam!" << endl;
 
-    sam = ZERO;
-    sue = ZERO;
+     sam = ZERO;
+     sue = ZERO;
 
-    cout << "sam has " << sam << " dollars and sue has " << sue << " dollars deposited. " << endl;
+     cout << "sam has " << sam << " dollars and sue has " << sue << " dollars deposited. " << endl;
 
-    cout << "Take $1 from each account. " << endl
-         << "Now ";
+     cout << "Take $1 from each account. " << endl
+          << "Now ";
 
-    sam = sam - 1;
-    sue = sue - 1;
-    cout << "Sam has " << sam << " dollars and Sue has " << sue << " dollars deposited." << endl
-         << "Lucky Sue!" << endl;
+     sam = sam - 1;
+     sue = sue - 1;
+     cout << "Sam has " << sam << " dollars and Sue has " << sue << " dollars deposited." << endl
+          << "Lucky Sue!" << endl;
 
-    return 0;
+     return 0;
 }
