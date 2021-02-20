799a67bcd396e26d0e7d8b0a4468a2963f586707
senlion <senliontec@163.com>
2021-02-20 08:56:13 +0800

    程序清单3.4

---
+++ section3/hexoct2.cpp
@@ -0,0 +1,29 @@
+// 程序清单3.4 hexoct2.cpp
+// hexoct2.cpp --display values in hex and octal
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int chest = 42;
+
+    int waist = 42;
+
+    int inseam = 42;
+
+    cout << "Monsieur cuts a striking figure!" << endl;
+
+    cout << dec;
+    cout << "chest = " << chest << " (decimal for 42)" << endl;
+
+    cout << hex;
+    cout << "waist = " << waist << " (hexadecimal for 42)" << endl;
+
+    cout << oct;
+    cout << "inseam = " << inseam << " (octal for 42)" << endl;
+
+    return 0;
+}
