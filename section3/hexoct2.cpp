6a1c5dcce95ca34e81e0d78a02831dc6e0ccfce2
senlion <senliontec@163.com>
2021-02-20 09:01:54 +0800

    程序清单3.4

--- section3/hexoct2.cpp
+++ section3/hexoct2.cpp
@@ -19,7 +19,7 @@ int main()
     cout << dec;
     cout << "chest = " << chest << " (decimal for 42)" << endl;
 
-    cout << hex;
+    cout << hex; //  不会在屏幕上显示任何内容，而只是修改cout显示整数的方式。hex相当与一条消息，告诉cout采取何种行为。
     cout << "waist = " << waist << " (hexadecimal for 42)" << endl;
 
     cout << oct;
