b7dea19527c96e82316cc89af363be1110e535bb
senlion <senliontec@163.com>
2021-02-20 10:27:58 +0800

    程序清单3.7

---
+++ section3/bondini.cpp
@@ -0,0 +1,18 @@
+// 程序清单3.7 bondini.cpp
+
+// bondini.cpp --using escape sequences
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    cout << "\aOperation \"HyperHype \" is now activated! " << endl;
+    cout << "Enter your agent code:_____________\b\b\b\b\b\b\b";
+    long code;
+    cin >> code;
+    cout << "\a You entered " << code << "..." << endl;
+    cout << "\aCode verified! Proceed with Plan Z3!\n";
+    return 0;
+}
