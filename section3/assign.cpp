8255e1ff1918908cb5ae56781020a8807e1bc46e
senliontec <72956049+senliontec@users.noreply.github.com>
2021-03-11 22:02:07 +0800

    程序清单3.13

---
+++ section3/assign.cpp
@@ -0,0 +1,24 @@
+// init.cpp --type changes on initialization
+
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    cout.setf(ios_base::fixed, ios_base::floatfield);
+
+    float tree = 3;  // int convert to float
+
+    int guess(3.9832); // double convert to int
+
+    int debt = 7.2E12; // result not defined in C++
+
+    cout<<"tree = "<<tree<<endl;
+    cout<<"guess = "<<guess<<endl;
+    cout<<"debt = "<<debt<<endl;
+
+    return 0;
+}
+
